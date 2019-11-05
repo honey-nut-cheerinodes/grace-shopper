99e51635b673eba72ff40ee7ab9567af7d8dd1d9
Windy <windy.seng@gmail.com>
2019-11-05 17:24:14 -0500

    Added backend files for store/product.js and server/api/products.js route.

---
+++ client/store/product.js
@@ -0,0 +1,34 @@
+import {initialState} from './index'
+import axios from 'axios'
+
+//action type for single product
+export const SINGLE_PRODUCT = 'SINGLE_PRODUCT'
+export const ALL_PRODUCTS = 'ALL_PRODUCTS'
+
+//create single product action creator
+export const singleProduct = product => {
+  return {
+    type: SINGLE_PRODUCT,
+    product
+  }
+}
+
+//single product thunk
+export const getSingleProduct = id => async dispatch => {
+  const {data} = await axios.get(`/api/products/${id}`)
+  dispatch(singleProduct(data))
+}
+
+//reducer
+const product = (state = initialState, action) => {
+  switch (action.type) {
+    case ALL_PRODUCTS:
+      return {...state, products: action.products}
+    case SINGLE_PRODUCT:
+      return {...state, product: action.product}
+    default:
+      return state
+  }
+}
+
+export default product
