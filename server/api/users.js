5aa1cd8ac35c96bc13113463edf28d07862488b6
Meredyth Houpos <meredyth.houpos@gmail.com>
2019-11-12 14:40:09 -0500

    commented out adminsOnly

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,15 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+// const adminsOnly = (req, res, next) => {
+//   if (!req.user.isAdmin) {
+//     const err = new Error('Not allowed!')
+//     err.status = 401
+//     return next(err)
+//   }
+//   next()
+// }
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
