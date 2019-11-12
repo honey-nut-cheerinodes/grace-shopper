20472b6a2550c1deed426cf8d03b6dbbcb3a61cd
Windy <windy.seng@gmail.com>
2019-11-12 16:09:56 -0500

    Fixed footer to bottom.

--- client/components/global-files/footer.js
+++ client/components/global-files/footer.js
@@ -3,25 +3,28 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div className="footer-bar">
-        <div id="footer-about">
-          <h4>About</h4>
-          <p>
-            Founded in 2019, Grace Hopper students wanted to find a way for pet
-            owners to show their pets the love they deserve in a fashionable and
-            sustainable way.
-          </p>
-        </div>
-        <div id="footer-address">
-          <h4>Address</h4>
-          <p>5 Hanover Sqaure, New York, New York</p>
-        </div>
-        <div id="footer-contact">
-          <h4>Contact</h4>
-          <p>Email us - hello@cloakanddogger.com</p>
-          <p>Call us - 000-000-0000</p>
+      <div className="wrapper">
+        <div className="footer-bar">
+          <div id="footer-about">
+            <h4>About</h4>
+            <p>
+              Founded in 2019, Grace Hopper students wanted to find a way for
+              pet owners to show their pets the love they deserve in a
+              fashionable and sustainable way.
+            </p>
+          </div>
+          <div id="footer-address">
+            <h4>Address</h4>
+            <p>5 Hanover Sqaure, New York, New York</p>
+          </div>
+          <div id="footer-contact">
+            <h4>Contact</h4>
+            <p>Email us - hello@cloakanddogger.com</p>
+            <p>Call us - 000-000-0000</p>
+          </div>
         </div>
       </div>
+      <div className="push" />
     </div>
   )
 }
